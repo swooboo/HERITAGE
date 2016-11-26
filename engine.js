318f760cfd1ebfe1c4f91196d8d3444530b5db1e
swooboo <swooboo@mail.ru>
2016-11-27 00:58:03 +0200

    Add copyright author and update Sylvie info

--- engine.js
+++ engine.js
@@ -2,7 +2,9 @@
 	@licstart  The following is the entire license notice for the
 	JavaScript code in this page.
 
-	Copyright (C) 2014 - 2015  SylvieLorxu <sylvie@contracode.nl>
+	Copyright (C) 2014 - 2015
+		Sylvia van Os <iamsylvie@openmailbox.org>
+		swooboo <swooboo@mail.ru>
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
